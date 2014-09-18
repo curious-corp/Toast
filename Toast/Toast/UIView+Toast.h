a47ac418b10d1cc7c248a8ca13c622d53e656976
Glen Wong <glen@xoocorp.com>
2014-09-18 16:15:07 -0700

    return UIView for additional mod

--- Toast/Toast/UIView+Toast.h
+++ Toast/Toast/UIView+Toast.h
@@ -32,11 +32,11 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 @interface UIView (Toast)
 
 // each makeToast method creates a view and displays it as toast
-- (void)makeToast:(NSString *)message;
-- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position;
-- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position image:(UIImage *)image;
-- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position title:(NSString *)title;
-- (void)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position title:(NSString *)title image:(UIImage *)image;
+- (UIView*)makeToast:(NSString *)message;
+- (UIView*)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position;
+- (UIView*)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position image:(UIImage *)image;
+- (UIView*)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position title:(NSString *)title;
+- (UIView*)makeToast:(NSString *)message duration:(NSTimeInterval)interval position:(id)position title:(NSString *)title image:(UIImage *)image;
 
 // displays toast with an activity spinner
 - (void)makeToastActivity;
